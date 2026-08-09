5a3f7293805b4893b38dd259278ada64b859c1bf
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-08-09 18:18:38 +0000

    feat: enhance LoginPage with username validation and availability check

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,42 +1,90 @@
 import { useState, useEffect } from "react";
 
-const LoginPage = (props) => {
+const LoginPage = (props: any) => {
+  const [username, setUsername] = useState("");
+  const [debouncedUsername, setDebouncedUsername] = useState("");
+  const [availability, setAvailability] = useState("");
+  const [usernameError, setUsernameError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
-  const { name } = props
 
-  const [username, setUsername] = useState("")
-  const [lateUsername, setLateUsername] = useState("")
-  const [loading, setLoading] = useState(false)
+  const handleUsername = (e) => {
+    e.preventDefault();
+
+    const username = e.target.value?.toLowerCase();
+
+    setUsername(username);
+    setAvailability("");
+    setUsernameError("")
+
+    if (!/^[a-z0-9]+$/.test(username)) {
+      setUsernameError("Username should must be alphanumerical")
+    } else if (username.length < 3) {
+      setUsernameError("Username length must be atleast 3 characters")
+    } else if (username.length > 20) {
+      setUsernameError("Username must be under 20 characters")
+    }
+  }
 
   useEffect(() => {
+    if (!/^[a-z0-9]{3,20}$/.test(username)) {
+      return;
+    } else if (username.length < 3) {
+      return;
+    } else if (username.length > 20) {
+      return;
+    }
 
-    if (!username) return;
-    const timer = setTimeout(async () => {
-      setLoading(true);
-      const res = await fetch(`http://localhost:3000/user/availability?username=${username}`);
-      setLoading(false)
-      if (res.ok) {
-        const data = await res.json();
-        if (data.success) {
-          setLateUsername(data.available);
-          console.log(data.available);
-        }
-      }
-    }, 3000);
+    const timerID = setTimeout(() => {
+      setDebouncedUsername(username)
+    }, 300)
 
     return () => {
-      clearTimeout(timer);
+      clearTimeout(timerID);
     }
   }, [username])
 
-  const handleChange = e => {
-    setUsername(e.target.value);
-  }
+  useEffect(() => {
+    if (!debouncedUsername) {
+      setAvailability("")
+      return;
+    }
+
+    try {
+      const sendApiRequest = async () => {
+        const url = `http://localhost:3000/user/availability?username=${debouncedUsername}`;
+        setIsLoading(true)
+        const response = await fetch(url);
+        setIsLoading(false)
+        if (response.ok) {
+          const responseData = await response.json();
+          const { data, message } = responseData;
+
+          const { username, available } = data;
+
+          if (available) {
+            setAvailability(available)
+            setUsernameError("")
+          }
+          else setUsernameError(message)
+
+        }
+      }
+
+      sendApiRequest()
+    } catch (e) {
+      console.log(e)
+    }
+
+  }, [debouncedUsername])
 
   return (
     <div className="">
-      <input value={username} onChange={handleChange} />
-      <p>{!loading ? lateUsername ? `${username} is available` : "Not available" : "loading"}</p>
+      <input value={username} onChange={handleUsername} />
+      {availability === true && <p>{debouncedUsername} is available</p>}
+      {availability === false && <p>{debouncedUsername} is not available</p>}
+      {usernameError && <p>{usernameError}</p>}
+      {isLoading && <p>Checking availability...</p>}
     </div>
   )
 }
