7e517bd42cc00beeb4f01fe42fdaa4cd1cd5dc34
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-08-11 18:17:48 +0000

    feat: integrate Tailwind CSS and refactor LoginPage component
    - Added Tailwind CSS dependencies to package.json and configured Vite to use Tailwind.
    - Removed existing CSS styles from App.css and index.css, replacing them with Tailwind utility classes.
    - Refactored LoginPage component to utilize Tailwind for styling and removed unnecessary state management for username availability.
    - Created a new DebouncedInput component for handling username input and availability checks.
    - Updated App component to use React Router for navigation.

---
+++ src/components/DebouncedInput.tsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+
+const DebouncedInput = () => {
+  const [username, setUsername] = useState("");
+  const [debouncedUsername, setDebouncedUsername] = useState("");
+  const [availability, setAvailability] = useState("");
+  const [usernameError, setUsernameError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  const handleUsername = (e) => {
+    e.preventDefault();
+
+    const username = e.target.value?.toLowerCase();
+
+    setUsername(username);
+    setAvailability("");
+    setUsernameError("")
+
+    if (!/^[a-z0-9]+$/.test(username)) {
+      setUsernameError("Username should must be alphanumerical")
+    } else if (username.length < 3) {
+      setUsernameError("Username length must be atleast 3 characters")
+    } else if (username.length > 20) {
+      setUsernameError("Username must be under 20 characters")
+    }
+  }
+
+  useEffect(() => {
+    if (!/^[a-z0-9]{3,20}$/.test(username)) {
+      return;
+    } else if (username.length < 3) {
+      return;
+    } else if (username.length > 20) {
+      return;
+    }
+
+    const timerID = setTimeout(() => {
+      setDebouncedUsername(username)
+    }, 300)
+
+    return () => {
+      clearTimeout(timerID);
+    }
+  }, [username])
+
+  useEffect(() => {
+    if (!debouncedUsername) {
+      setAvailability("")
+      return;
+    }
+
+    try {
+      const sendApiRequest = async () => {
+        const url = `http://localhost:3000/user/availability?username=${debouncedUsername}`;
+        setIsLoading(true)
+        const response = await fetch(url);
+        setIsLoading(false)
+        if (response.ok) {
+          const responseData = await response.json();
+          const { data, message } = responseData;
+
+          const { username, available } = data;
+
+          if (available) {
+            setAvailability(available)
+            setUsernameError("")
+          }
+          else setUsernameError(message)
+
+        }
+      }
+
+      sendApiRequest()
+    } catch (e) {
+      console.log(e)
+    }
+
+  }, [debouncedUsername])
+
+  return (
+    <div className="">
+      <input value={username} onChange={handleUsername} />
+      {availability === true && <p>{debouncedUsername} is available</p>}
+      {availability === false && <p>{debouncedUsername} is not available</p>}
+      {usernameError && <p>{usernameError}</p>}
+      {isLoading && <p>Checking availability...</p>}
+    </div>
+  )
+}
+
+export default DebouncedInput
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,90 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
+import {Link} from "react-router"
 
-const LoginPage = (props: any) => {
-  const [username, setUsername] = useState("");
-  const [debouncedUsername, setDebouncedUsername] = useState("");
-  const [availability, setAvailability] = useState("");
-  const [usernameError, setUsernameError] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-
-
-  const handleUsername = (e) => {
-    e.preventDefault();
-
-    const username = e.target.value?.toLowerCase();
-
-    setUsername(username);
-    setAvailability("");
-    setUsernameError("")
-
-    if (!/^[a-z0-9]+$/.test(username)) {
-      setUsernameError("Username should must be alphanumerical")
-    } else if (username.length < 3) {
-      setUsernameError("Username length must be atleast 3 characters")
-    } else if (username.length > 20) {
-      setUsernameError("Username must be under 20 characters")
-    }
-  }
-
-  useEffect(() => {
-    if (!/^[a-z0-9]{3,20}$/.test(username)) {
-      return;
-    } else if (username.length < 3) {
-      return;
-    } else if (username.length > 20) {
-      return;
-    }
-
-    const timerID = setTimeout(() => {
-      setDebouncedUsername(username)
-    }, 300)
-
-    return () => {
-      clearTimeout(timerID);
-    }
-  }, [username])
-
-  useEffect(() => {
-    if (!debouncedUsername) {
-      setAvailability("")
-      return;
-    }
-
-    try {
-      const sendApiRequest = async () => {
-        const url = `http://localhost:3000/user/availability?username=${debouncedUsername}`;
-        setIsLoading(true)
-        const response = await fetch(url);
-        setIsLoading(false)
-        if (response.ok) {
-          const responseData = await response.json();
-          const { data, message } = responseData;
-
-          const { username, available } = data;
-
-          if (available) {
-            setAvailability(available)
-            setUsernameError("")
-          }
-          else setUsernameError(message)
-
-        }
-      }
-
-      sendApiRequest()
-    } catch (e) {
-      console.log(e)
-    }
-
-  }, [debouncedUsername])
-
+const LoginPage = () => {
   return (
-    <div className="">
-      <input value={username} onChange={handleUsername} />
-      {availability === true && <p>{debouncedUsername} is available</p>}
-      {availability === false && <p>{debouncedUsername} is not available</p>}
-      {usernameError && <p>{usernameError}</p>}
-      {isLoading && <p>Checking availability...</p>}
+    <div className="h-screen p-8">
+      <div className="h-full grid grid-cols-2 ">
+        <div className="border-2 border-amber-300">
+
+        </div>
+        <div className=" flex flex-col px-30 py-10">
+          <div className="">
+            <h1 className="text-3xl font-medium">Create an account</h1>
+            <p className="text-sm text-gray-500 mt-2">Already have an account? <Link className="underline" to="/login">Log in</Link></p>
+            <form className="grid mt-10 grid-cols-2 gap-6">
+              <div>
+                <label className="text-gray-400 text-sm font-medium" htmlFor="firstName">First Name</label>
+                <input className="p-3 w-full outline-none text-gray-900 placeholder:text-gray-400 text-sm font-medium border-gray-300 border bg-gray-200 rounded-md" id="firstName" placeholder="First Name" type="text"/>
+              </div>
+              <div>
+                <label className="text-gray-400 text-sm font-medium" htmlFor="lastName">Last Name</label>
+                <input className="p-3 w-full text-gray-900 placeholder:text-gray-400 text-sm font-medium outline-none border-gray-300 border bg-gray-200 rounded-md" id="lastName" placeholder="Last Name" type="text" />
+              </div>
+              <div className="col-span-2">
+                <label className="text-gray-400 text-sm font-medium" htmlFor="username">Username</label>
+                <input className="p-3 text-gray-900 placeholder:text-gray-400 text-sm font-medium outline-none border-gray-300 border w-full bg-gray-200 rounded-md" id="username" placeholder="Username" type="text" />
+              </div>
+              <div className="col-span-2">
+                <label className="text-gray-400 text-sm font-medium" htmlFor="email">Email</label>
+                <input className="w-full p-3 outline-none border-gray-300 text-sm font-medium text-gray-900 placeholder:text-gray-400 border bg-gray-200 rounded-md" id="email" placeholder="Email" type="email" />
+              </div>
+              <div className="col-span-2">
+                <label className="text-gray-400 text-sm font-medium" htmlFor="password">Password</label>
+                <input className="w-full p-3 border-gray-300 text-sm font-medium outline-none border text-gray-900 placeholder:text-gray-400 bg-gray-200 rounded-md" id="password" placeholder="Password" type="password" />
+              </div>
+
+              <button className="col-span-2 font-medium mt-5 bg-violet-600 hover:bg-violet-500 border rounded-md outline-none text-white p-3 cursor-pointer" type="submit">Create account</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
